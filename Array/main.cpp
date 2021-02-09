4303a27f11d694ff746a5313ae5b7bfcf51d39bc
KevinVillania <75327733+KevinVillania@users.noreply.github.com>
2021-02-09 21:24:27 +0800

    1

--- Array/main.cpp
+++ Array/main.cpp
@@ -9,30 +9,29 @@ using namespace std;
 int main(){
 
 int kevinArray[3];
-string cars[5] = {"BMW", "Mitsubishi", "Volvo"};
-int kevin = 5;
+string cars[3] = {"BMW", "Mitsubishi", "Volvo"};
+string kevin2 = "Hey yow whats up";
 
 kevinArray[0] = 5;
 kevinArray[1] = 4;
 kevinArray[2] = 3;
-
+/*
 for(int i = 0; i<(sizeof(kevinArray)/sizeof(kevinArray[0])); ++i){
 
     cout << kevinArray[i] << endl;
 
-}
+}*/
 
-cout << sizeof(kevinArray) << endl;
-cout << sizeof(kevinArray[0]) << endl;
+cout << sizeof(cars) << endl;
+cout << sizeof(cars[0]) << endl;
 
 int arrSize;
-
-arrSize = sizeof(kevinArray)/sizeof(kevinArray[0]);
 int arrLength;
 
-arrLength = sizeof(kevin);
+arrSize = sizeof(kevinArray)/sizeof(kevinArray[0]);
+//arrLength = sizeof(kevin) << endl;
 
-cout << cars.size();
+cout << kevin2.size() << endl;
 
 cout << "The number of elements inside kevinArray is " << arrSize;
 
