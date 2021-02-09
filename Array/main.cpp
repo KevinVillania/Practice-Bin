2dc2e4e3e3d1de363f81fa834c2ba4d01268a803
KevinVillania <75327733+KevinVillania@users.noreply.github.com>
2021-02-09 22:17:06 +0800

    array practice

--- Array/main.cpp
+++ Array/main.cpp
@@ -1,40 +1,14 @@
 #include <iostream>
 #include <array>
-#include <vector>
-#include <algorithm>
-#include <iomanip>
 #include <string>
-using namespace std;
 
 int main(){
 
-int kevinArray[3];
-string cars[3] = {"BMW", "Mitsubishi", "Volvo"};
-string kevin2 = "Hey yow whats up";
+std::array<int, 5> cars;
 
-kevinArray[0] = 5;
-kevinArray[1] = 4;
-kevinArray[2] = 3;
-/*
-for(int i = 0; i<(sizeof(kevinArray)/sizeof(kevinArray[0])); ++i){
+cars[0] = 4;
 
-    cout << kevinArray[i] << endl;
-
-}*/
-
-cout << sizeof(cars) << endl;
-cout << sizeof(cars[0]) << endl;
-
-int arrSize;
-int arrLength;
-
-arrSize = sizeof(kevinArray)/sizeof(kevinArray[0]);
-//arrLength = sizeof(kevin) << endl;
-
-cout << kevin2.size() << endl;
-
-cout << "The number of elements inside kevinArray is " << arrSize;
-
-
+std::cout << cars.size() << std::endl;
+std::cout << cars[0];
 
 }
