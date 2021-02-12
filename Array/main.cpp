4971e6f9df72048a4a59fb3aa0c974ec1cbb4775
KevinVillania <75327733+KevinVillania@users.noreply.github.com>
2021-02-12 18:16:42 +0800

    1

--- Array/main.cpp
+++ Array/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <algorithm>
-#include <array>
 #include <functional>
+#include <vector>
 using namespace std;
 
 int main(){
 
 int num;
+int laki;
 int kevinArray[8] {23,4,32,25,7,54,111,9};
+
+//laki = kevinArray.size();
 
 num = sizeof(kevinArray)/sizeof(kevinArray[0]);
 
-sort(kevinArray,kevinArray+num);
+sort(kevinArray,kevinArray+num, less<int>());
 
 for(auto a: kevinArray){
 
