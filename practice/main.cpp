b484cbca70e59a98a3e0ed1e6d66e3e3a31304be
KevinVillania <75327733+KevinVillania@users.noreply.github.com>
2021-02-25 12:34:41 +0800

    Template
    Template for same data type

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,43 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
-class Enemy{
-    public:
-        virtual void attack(){};
-        //virtual function when called checks functions in derived classes
-};
 
-class Ninja: public Enemy{
-
-public:
-    void attack(){
-
-        cout << "Shurikennn tosss!!" << endl;
-    }
-};
-
-class Monster: public Enemy{
-
-    public:
-        void attack(){
-
-            cout << "Rawr rawr" << endl;
-        }
-};
+//generic function adds two number with THE SAME data type
+template <class kevin>
+kevin addCalculator(kevin x, kevin y){
+    return x+y;
+}
 
 int main(){
 
-    //Creating an object for Ninja/monster class
-    Ninja n;
-    Monster m;
+int x=0; int y=0;
+int sum;
 
-    //creating a pointer for ninja and monster class
-    Enemy *enemy1 = &n;
-    Enemy *enemy2 = &m;
+    cin >> x >> y;
 
-    enemy1->attack();
-    enemy2->attack();
+    sum = addCalculator(x,y);
 
-
+    cout << sum;
 }
