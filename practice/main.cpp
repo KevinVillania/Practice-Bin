b8b9f7ebc94c3cb7cbc71f429964475e9f54e572
KevinVillania <75327733+KevinVillania@users.noreply.github.com>
2021-02-25 19:14:05 +0800

    array

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 using namespace std;
 
-
-//generic function adds two number with THE SAME data type
-template <class kevin>
-kevin addCalculator(kevin x, kevin y){
-    return x+y;
-}
-
 int main(){
 
-int x=0; int y=0;
-int sum;
+int array1[5] {5,9,2,3,4};
+int array2[2][3] {{2,3,4},{3,4,5}};
 
-    cin >> x >> y;
+//prints array1
+for(auto a: array1){
 
-    sum = addCalculator(x,y);
+    cout << a << " ";
+}
 
-    cout << sum;
+//prints multidimensional array
+for(int row=0; row<2; ++row){
+
+    for(int column=0; column<3; ++column){
+
+        cout << array2[row][column] << " ";
+    }
+    cout << endl;
+}
+
+//get number of elements of array1
+int numElements;
+
+numElements = sizeof(array1)/sizeof(array1[0]);
+
+cout << "Number of elements inside array1 is "<< numElements << endl;
+
+//get number of elements in multidimensional array2
+int numElements2;
+
+numElements2 = sizeof(array2)/sizeof(array2[0][0]);
+cout << "Number of elements inside array2 is "<< numElements2;
+
+
+
 }
