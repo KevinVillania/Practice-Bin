fd9a4530b4f054b13fd6b486df8184c91c4d0e2c
KevinVillania <75327733+KevinVillania@users.noreply.github.com>
2021-03-10 08:34:13 +0800

    March COmmit

--- Random C++ Practices/main.cpp
+++ Random C++ Practices/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
-#include "oreo.h"
-#include "humphrey.h"
+using namespace std;
 
 int main(){
 
-    humphrey hm;
-    //hm.bark();
+    int year;
 
-    oreo ob;
-    ob.shout();
+    cout << "Leap year detector\n";
+    cout << "Enter a year: ";
+    cin >> year;
 
-    hm.stand();
+    if(year%4 == 0){
+
+        if(year % 100 == 0){
+
+            if(year % 400 == 0){
+
+                cout << year << " is a leap year!";
+            }else{
+                    cout << year << " is not a leap year!";
+            }
+
+    }else{
+
+        cout << year << " is a leap year!";
+    }
 
 
+}else{
+
+    cout << year << " is not a leap year!";
+}
 }
