e7ea125dfed46ec437bace1ee9977b27c034a0ce
Kevin <khsvillania@iskolarngbayan.pup.edu.ph>
2021-03-24 18:19:57 +0800

    practicing data types
    INT-
    short int
    long int
    int
    signed int
    unsigned int
    float
    double
    char

---
+++ Random C++ Practices/Vectors/Linked List/Linked List/data types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    unsigned short int  beta = 65535;
+
+    cout << "Size of Integers" << endl << endl;
+    cout << "int " << sizeof(int) << endl;
+    cout << "Long int " << sizeof(long int) << endl;
+    cout << "long long int " << sizeof(long long int) << endl;
+    cout << "Short int " << sizeof(short int) << endl;
+    cout << "Unsigned Long int " << sizeof(unsigned long int) << endl;
+    cout << "Unsigned Short int " << sizeof(unsigned short int) << endl << endl;
+
+    cout << "float " << sizeof(float) << endl;
+
+    cout << "char " << sizeof(char) << endl;
+    cout << "double " << sizeof(double) << endl;
+    cout << "long double " << sizeof(long double) << endl;
+
+
+}
