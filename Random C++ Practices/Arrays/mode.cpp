d3b33d3cc23a1c47d9a13317db4ed27f36b5b9f8
Kevin <khsvillania@iskolarngbayan.pup.edu.ph>
2021-03-28 15:57:49 +0800

    Finding Mode
    rework on number of elements for better understanding

--- Random C++ Practices/Arrays/mode.cpp
+++ Random C++ Practices/Arrays/mode.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void modeCalc(int arr[], int num){
+void modeCalc(int arr[], int numElem){
 
     int counter =1;
     int max=0;
     int mode=arr[0];
 
-    for(int i=0; i < num-1; i++){
+    for(int i=0; i < numElem-1; i++){
 
         if(arr[i] == arr[i+1]){
 
@@ -26,7 +26,7 @@ void modeCalc(int arr[], int num){
 
 int main(){
 
-int arr[10] {10,10,10,20,20,30,40,50,60,10};
+int arr[10] {1,1,1,20,20,30,40,50,60,10};
 modeCalc(arr,10);
 
 }
